9727d8911871e6bd235b9c1389d27335d55f6224
SimpleMathematics <services@basicsplus.com>
2018-04-17 00:17:53 -0400

    Update FBA_Script.py

--- Scripts/P.O. to GOTO/FBA_Script.py
+++ Scripts/P.O. to GOTO/FBA_Script.py
@@ -1,46 +1,24 @@
 # P.O. to Transfer Order
-
 import json
-from pprint import pprint
-from html.parser import HTMLParser
 
-with open('LECR (12th-123).txt', mode='r') as rf:
-
-	notedata = json.load(open('LECR (12th-123).txt'))
+with open('LECR (12th-123).json', mode="r") as rf:
+	note = {}
+	newnote = []
+	notedata = json.load(rf)
 	for shipments in notedata['shipment']:
-		sku = shipments['sku'][0:]
-		fba = shipments['fba'][0:]
-		qty = shipments['qty'][0:]
-		print(sku, fba, qty)
+		note['sku'] = shipments['sku'][0:]
+		note['fba'] = shipments['fba'][0:]
+		note['qty'] = shipments['qty'][0:]
+		newnote.append(note['sku'][0:])
+
+with open('testing.html', mode="w") as wf:
+
+	for notes in note:
+		wf.write(note['sku'])
 
 
-with open('LECR (12th-123).html', mode='r+') as wf:
-	
-	temp = wf.read()
-	tempdata = json.dumps(temp, indent=2)
-	# print(tempdata)
+		print(note['sku'])
+		# print(note['sku'], note['fba'], note['qty'])
 
-	class MyHTMLParser(HTMLParser):
-		def handle_starttag(self, tag, attrs):
-			print("start tag:", tag)
+# print(notedata, type(notedata))
 
-		def handle_endtag(self, tag):
-			print("end tag :", tag)
-
-		def handle_data(self, data):
-			print("Encountered some data  :", data)
-		
-parser = MyHTMLParser()
-tempdatasort = parser.feed(tempdata)
-
-
-
-
-
-
-
-
-
-
-# new_string = json.dumps(data, indent=2, sort_keys=True)
-# print (new_string)
